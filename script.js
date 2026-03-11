3466dc36070d0176bb08229ec45b8cfb6b1b336e
dude1ko-eng <dude1ko@gmail.com>
2026-03-11 14:43:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,19 +14,3 @@ function addTask() {
   document.getElementById('taskList').appendChild(li);
   input.value = "";
 
-function addTask() {
-  const input = document.getElementById('taskInput');
-  const task = input.value.trim();
-  if(task === "") return;
-
-  const li = document.createElement('li');
-  li.textContent = task;
-
-  li.onclick = function() {
-    this.style.textDecoration = "line-through";
-  }
-
-  document.getElementById('taskList').appendChild(li);
-  input.value = "";
-
-}
